f2827ba5efb6c3353e64e89055617476e49e5485
Pirhoo <pierre.romera@gmail.com>
2012-06-09 16:31:36 +0200

    Implementation du scroll

--- public/js/global.js
+++ public/js/global.js
@@ -2,4 +2,49 @@
 $(window).load(function() {
 
 
-})
+	function callbackFacebook() {
+
+	}
+
+
+	function bindEvents() {
+
+		$('#connect').click(function(event) {			
+
+			wsh.exec('oauth -provider facebook', function(json) {				
+				$('body').append(json.view);
+				syncSetCallback(callbackFacebook);
+			});
+
+			event.preventDefault();
+		});
+
+
+		// Slider's commands
+		$(".slider-nav").delegate("a", "click", function() {
+			
+			// Next slide
+			if( $(this).hasClass("next") ) {
+				$(".js-slider").data("µSlide").slideTo("next");
+			// Previous slide
+			} else if( $(this).hasClass("prev") ) {
+				$(".js-slider").data("µSlide").slideTo("previous");
+			}
+
+		})
+
+	}
+
+
+	(function init() {
+
+		// All user's events
+		bindEvents();
+
+		// Initialize the slide
+		$(".js-slider").µSlide({child_class: ".slide"});
+
+	})();
+
+
+});
