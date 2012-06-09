76dda049eb66527b29713d7bb546801485385d01
Pirhoo <pierre.romera@gmail.com>
2012-06-09 21:23:29 +0200

    Controller pour les trajets

---
+++ application/controllers/SetupController.php
@@ -0,0 +1,20 @@
+<?php
+
+class SetupController extends Zend_Controller_Action
+{
+    /**
+     * Initialize action controller here
+     */
+    public function init()
+    {
+        
+    }
+
+    /**
+     * action body
+     */
+    public function indexAction()
+    {
+    }
+}
+?>
