383389be15082a7cd5ac270808332bfdac5336ba
Arnaud Richard <bumpmann@gmail.com>
2012-06-10 03:16:22 +0200

    Signed-off-by: Arnaud Richard <bumpmann@gmail.com>

--- application/controllers/TestController.php
+++ application/controllers/TestController.php
@@ -22,6 +22,29 @@ class TestController extends Zend_Controller_Action
         require_once APPLICATION_PATH . '/services/Sncf.php';
         $sncf = new Sncf();
         $res = $sncf->autocompleteGare("mass");
+        var_dump($res);die();
+    }
+
+    public function corrAction()
+    {
+        require_once APPLICATION_PATH . '/services/Sncf.php';
+        $sncf = new Sncf();
+        $gare1 = $sncf->autocompleteGare("igny");
+        $gare2 = $sncf->autocompleteGare("pont-cardinet");
+        $gare1 = $gare1[0];
+        $gare2 = $gare2[0];
+        $res = $sncf->hasCorrespondance($gare1[1], $gare2[1]);
+        var_dump($res);die();
+    }
+    public function searchtrainAction()
+    {
+        require_once APPLICATION_PATH . '/services/Sncf.php';
+        $sncf = new Sncf();
+        $gare = $sncf->autocompleteGare('igny');
+        $gare2 = $sncf->autocompleteGare('VERSAILLES-CHANTIERS');
+        $res = $sncf->searchTrains($gare[0], $gare2[0], time() - 3600 * 12);
+        //var_dump($res);
+        die();
     }
 }
 ?>
--- application/services/Sncf.php
+++ application/services/Sncf.php
@@ -1,7 +1,8 @@
 <?php /* todo:
-- autocompletion gares -> data
-- correspondances/ou pas entre deux gares                             |
-- correspondances/ou pas entre plusieurs gares (chemin possible ?)    |-> microservices
+ok - autocompletion gares -> data
+
+later - correspondances/ou pas entre deux gares                             |
+later - correspondances/ou pas entre plusieurs gares (chemin possible ?)    |-> microservices
 
 - départs de trains (sur un trajet) dans une tranche horaire | jours/semaine (dates?) -> microservices
 - ajouter la durée du trajet à la tranche horaire (déterminer la durée avec correspondances) -> microservices
@@ -11,10 +12,12 @@
 
 class Sncf
 {
+	private $_timespan = 600;
+
 	private function getGares()
 	{
 		$cache = Zend_Registry::get('cache');
-        $cache_id = 'gares__eq';
+        $cache_id = 'gares_eq';
         if ( ! ($data = $cache->load($cache_id)))
         {
             require_once APPLICATION_PATH . '/../library/Webshell.php';
@@ -29,13 +32,33 @@ class Sncf
 				if ( isset($d[1], $d[3], $d[9], $d[10], $d[0]) && ! in_array($d[1], $tmp))
 				{
 					$tmp[] = $d[1];
-					$tmp2[] = array($d[1], $d[3], $d[9], $d[10], $d[0]);
+					$tmp2[] = array($d[1], 'DUA' . substr($d[3], 0, strlen($d[3]) - 1), $d[9], $d[10], $d[0]);
 				}
 			}
 			$data = $tmp2;
             $cache->save($data, $cache_id, array('gare'));
         }
         return $data;
+	}
+
+	public function hasCorrespondance($start, $end)
+	{
+		return false; // /!\ ignore correspondances for now
+/*
+		$cache = Zend_Registry::get('cache');
+        $cache_id = 'gares__correspondance' . rand();
+        if ( ! ($data = $cache->load($cache_id)))
+        {
+            require_once APPLICATION_PATH . '/../library/Webshell.php';
+			$webshell = Webshell::getInstance();
+			$res = Zend_Json::decode($webshell->exec('@sncf linelist -Action LineList -StopAreaExternalCode "' . $start . ';' . $end . '|and"'));
+			$data = $res[0]['data'];
+var_dump('pl');
+			var_dump($data);die();
+
+            $cache->save($data, $cache_id, array('gare'));
+        }
+        return $data;*/
 	}
 
 	public function prepareAutocomplete($name)
@@ -49,7 +72,47 @@ class Sncf
 		$gares = array_filter($gares, function($gare) use($name, $namelen) {
 			return strtolower(substr($gare[0], 0, $namelen)) == strtolower($name);
 		});
-		return $gares;
+
+		return array_values($gares);
+	}
+
+	public function searchTrains($gare, $gare2, $timestamp, $ponctual = false)
+	{
+		$cache = Zend_Registry::get('cache');
+        $cache_id = 'sncf_searchtrain_' . $timestamp . '_' . $gare[1] . $gare2[1] . $ponctual;
+        if ( ! ($data = $cache->load($cache_id)))
+        {
+        	$timestart = date('H|i', $timestamp - $this->_timespan);
+        	$timeend = date('H|i', $timestamp + $this->_timespan);
+
+        	$extra = "";
+        	if ($ponctual)
+        	{
+        		$datestart = date('Y|m|d', $timestamp);
+        		$extra = " -Date '" . $datestart . "'";
+        	}
+
+            require_once APPLICATION_PATH . '/../library/Webshell.php';
+			$webshell = Webshell::getInstance();
+			$res = Zend_Json::decode($webshell->exec('@sncf vehiclejourneylist -Action VehicleJourneyList -StopAreaExternalCode "' . $gare[1] . ';' . $gare2[1] . '|and" -StartTime "' . $timestart . '" -EndTime "' . $timeend . '"' . $extra));
+			$data = $res[0]['data']['ActionVehicleJourneyList']['VehicleJourneyList'];
+
+            $cache->save($data, $cache_id, array('gare'));
+        }
+        return $data;
+	}
+
+	public function searchTravel($travel, $from)
+	{
+
+	}
+
+	public function searchTravels($travels, $from)
+	{
+		$results = array();
+		foreach ($travels as $travel)
+			$results[] = $this->searchTravel($travel, $from);
+		return $results;
 	}
 
 	public function autocompleteGare($name) {
