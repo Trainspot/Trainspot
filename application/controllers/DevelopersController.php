f323180ce87d9e8e3fed276e858a6df52d5c6da5
Arnaud Richard <bumpmann@gmail.com>
2012-06-10 17:07:50 +0200

    Added /developers
    Signed-off-by: Arnaud Richard <bumpmann@gmail.com>

---
+++ application/controllers/DevelopersController.php
@@ -0,0 +1,20 @@
+<?php
+
+class DevelopersController extends Zend_Controller_Action
+{
+    /**
+     * Initialize action controller here
+     */
+    public function init()
+    {
+        
+    }
+
+    /**
+     * action body
+     */
+    public function indexAction() {
+    }
+
+}
+?>
